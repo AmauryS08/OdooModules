23926879ddf4635597796e34a975062bf36bd982
AmauryS08 <57544845+AmauryS08@users.noreply.github.com>
2021-04-02 16:59:50 +0200

    Add files via upload

--- journamod/models/journalier.py
+++ journamod/models/journalier.py
@@ -5,19 +5,33 @@ from odoo.exceptions import UserError, ValidationError
 class journalier(models.Model):
     _name = 'journamod.journalier'
     _description = 'Menu model'
+    _rec_name = 'sequence_id'
 
+    sequence_id = fields.Char(string='sequence', readonly=True)
     nom = fields.Char(string='Nom', Requiered=True)
     prenom = fields.Char(string='Prenom', Requiered=True)
-    date_naissance = fields.Date(string='Date de naissance', Requiered = True)
-    user_id = fields.Integer(string='Identification Utilisateur', Requiered = True)
+    date_naissance = fields.Date(string='Date de naissance', Requiered=True)
+    user_id = fields.Integer(string='Identification Utilisateur', Requiered=True)
     genre = fields.Selection([
         ('man', 'Homme'),
         ('woman', 'Femme'),
         ('other', 'Autre')
     ], string="Genre", default='man')
     state = fields.Selection([
-        ('available','Disponible'),
-        ('busy','Occupe'),
-        ('secret', 'Non renseigne')
+        ('available', 'Disponible'),
+        ('busy', 'Occupe'),
     ], string="Etat", default='available')
 
+    @api.model
+    def create(self, vals):
+        seq = self.env['ir.sequence'].next_by_code('journamod') or '/'
+        vals['sequence_id'] = seq
+        return super(journalier, self).create(vals)
+
+    def occupe_progressbar(self):
+        self.write({'state': 'busy', })
+
+    def libre_progressbar(self):
+        self.write({'state': 'available'})
+
+    _sql_constraints = [('id_unique', 'UNIQUE(user_id)', 'Numero deja existant.')]
