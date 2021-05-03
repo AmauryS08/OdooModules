9cab6ea15346e27486859ff8c20b1152abbcdaef
AmauryS08 <57544845+AmauryS08@users.noreply.github.com>
2021-05-03 13:35:57 +0200

    new module

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'product_search_filter',
+    'version': '0.1',
+    'description': 'Modifications module product',
+    'category': 'Uncategorized',
+    'author': 'Oci',
+    'depends': ['base','product'],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': True,
+}
