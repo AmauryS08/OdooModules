22fc50f296626e61246e723485d71a96e5de1f75
AmauryS08 <57544845+AmauryS08@users.noreply.github.com>
2021-05-20 09:50:30 +0200

    Delete sale_edit directory

--- sale_edit/models/sale_edit.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models, api
-
-
-class sale_edit(models.Model):
-    _name = 'sale_edit.sale_edit'
-    _description = 'Modification module sale'
-    _inherit = 'account.move'
-
-    name = fields.Char()
